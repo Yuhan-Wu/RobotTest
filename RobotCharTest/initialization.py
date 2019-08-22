41505667460689dbfa87cf057ece72e3fc906f43
Yuhan Wu <iriswuyuhan@gmail.com>
2019-08-22 13:02:41 -0600

    change initialization

--- RobotCharTest/initialization.py
+++ RobotCharTest/initialization.py
@@ -14,6 +14,9 @@ key=None
 velocity=10
 
 def detect_collision():
+    failing=pygame.Rect.colliderect(bullet_from_robot.rect,cowboy.rect)
+    if failing:
+        print("collide1")
 
     for bodypart in robot.bodyparts:
         if pygame.Rect.colliderect(bullet.rect,bodypart.rect):
@@ -34,17 +37,25 @@ def main():
     failing = False
 
     global robot
-    head=Head()
+    # head=Head()
     body=Body()
     bodyparts=[]
-    bodyparts.append(head)
+    # bodyparts.append(head)
     bodyparts.append(body)
     robot=Robot(bodyparts)
     robot.draw(win)
 
+    global cowboy
+    cowboy=Cowboy()
+    cowboy.draw(win)
+
     global bullet
     bullet=Bullet()
     bullet.draw(win)
+
+    global bullet_from_robot
+    bullet_from_robot=Bullet(position=(300,300))
+    bullet_from_robot.draw(win)
 
     pygame.display.update()
 
@@ -54,16 +65,28 @@ def main():
        for event in pygame.event.get():
            if event.type==pygame.QUIT:
                 run=False
+
+       # change bullet position
        bullet.update_position(velocity)
+       bullet_from_robot.update_position(-2*velocity)
+
+       detect_collision()
+
        win.blit(pygame.image.load("jellyfish.jpg"), (0, 0))
        robot.draw(win)
        bullet.draw(win)
-       if winning:
+       bullet_from_robot.draw(win)
+       cowboy.draw(win)
+       if winning or failing:
            break
        pygame.display.update()
 
     if winning and not(failing):
+        print("win")
+        pass
 
+    if failing:
+        print("fail")
         pass
 
     pass
