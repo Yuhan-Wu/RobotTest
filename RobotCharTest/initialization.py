4cae4e2c42a3739c1324a69acc45da68c6dfbb1a
Deeprashmi Nath <deepnath01@gmail.com>
2019-08-28 17:35:51 -0600

    Added Drone Code and Modified Initialization to reflect Drone Functionality

--- RobotCharTest/initialization.py
+++ RobotCharTest/initialization.py
@@ -10,6 +10,7 @@ from RobotCharTest.Cowboy import Cowboy
 from RobotCharTest.RobotGun import RobotGun
 from RobotCharTest.Bullet import Bullet
 from RobotCharTest.AmmoManager import AmmoManager
+from RobotCharTest.Drone import Drone
 
 
 win_width=1200
@@ -23,16 +24,29 @@ clock = pygame.time.Clock()
 key=None
 
 # CONFIG PARAMETER
-bullet_speed=40
+bullet_speed=5
 bullet_position = (220, win_height-160)
-cowboy_gun_rot_speed = 20
-robot_gun_rot_speed = -0.1
-
+cowboy_gun_rot_speed = 1
+robot_gun_rot_speed = 0
+drone_speed = 1
+global canDamage
+canDamage = 0
 
 def detect_collision():
+    global canDamage
     winning=False
+
+    droneCollision = False
+    for drone in drones:
+        if drone.isActive and drone.rect[0] == 150:
+            droneCollision = True
+        if droneCollision:
+            break
+    #pygame.Rect.collidelist
+
     failing= pygame.Rect.colliderect(robot_bullet.rect, cowboy.rect) or \
-             pygame.Rect.colliderect(cowboy_bullet[ammo_manager.get_ammo_index()].rect, cowboy.rect)
+             pygame.Rect.colliderect(cowboy_bullet[ammo_manager.get_ammo_index()].rect, cowboy.rect) or \
+             droneCollision
 
     if failing:
         cowboy.lose_sound()
@@ -47,16 +61,26 @@ def detect_collision():
         pass
         #pygame.time.delay(1000)
 
+    for drone in drones:
+        for bullet in cowboy_bullet:
+            if drone.isActive is True and pygame.Rect.colliderect(drone.rect, bullet.rect):
+                drone.isActive = False
+                canDamage += 1
+                print("canDamage : " + str(canDamage))
+                #decrease armor image
+                pass
+
     for bodypart in robot.bodyparts:
         for b in cowboy_bullet:
             if pygame.Rect.colliderect(b.rect, bodypart.rect):
                 b.reset()
                 robot.sound()
-                winning = robot.decrease_health(bodypart)
-                print("collide")
+                if canDamage == 4:
+                    winning = robot.decrease_health(bodypart)
+                    print("collide")
                 if winning:
                     pygame.mixer.music.load(win_noise)
-                    pygame.mixer.music.play(loops=3, start=0)
+                    pygame.mixer.music.play(loops=1, start=0)
                     win_screen = pygame.image.load('win.png').convert_alpha()
                     win.blit(win_screen, [0, 0])
                     print("win")
@@ -74,6 +98,21 @@ def main():
     pygame.display.set_caption("Cowboy vs. Robot")
     bg1=pygame.image.load("SpaceBackground(Rapid).png").convert_alpha()
     bg2=pygame.image.load("alien_land_v1.png").convert_alpha()
+
+    global drones
+    drones = []
+    drone_one = Drone(path="drone.png", position=(win_width, 0))
+    drone_two = Drone(path="drone.png", position=(1100, 100))
+    drone_three = Drone(path="drone.png", position=(win_width, 200))
+    drone_four = Drone(path="drone.png", position=(1100, 300))
+    drones.append(drone_one)
+    drones.append(drone_two)
+    drones.append(drone_three)
+    drones.append(drone_four)
+    for drone in drones:
+        if drone.isActive is True:
+            drone.draw(win)
+        pass
 
     global robot
     head=Head(path="robot_head_v1.png",position=(win_width-300,win_height-200))
@@ -115,7 +154,7 @@ def main():
     pygame.display.update()
 
     angle = 0
-
+    drone_move_counter = 3
     run=True
     while run:
        #pygame.time.delay(50)
@@ -136,8 +175,19 @@ def main():
        win.blit(bg2, (0, 0))
 
        if not (winning or failing):
-           robot.draw(win)
-           cowboy.draw(win)
+        robot.draw(win)
+        cowboy.draw(win)
+        for drone in drones:
+            if drone.isActive is True:
+                drone.draw(win)
+
+
+        if not (winning or failing) and drone_move_counter == 3:
+            for drone in drones:
+                if drone.isActive is True:
+                    drone.move(drone_speed)
+            drone_move_counter = -1
+        drone_move_counter += 1
 
        if not robot_gun.rotate(robot_gun_rot_speed, 320):
            robot_bullet.velocity=(-1, 0) * bullet_speed
